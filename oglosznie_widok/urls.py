b82acf410f73fa07abdbdcf0177f332ed463ea95
shaki886 <shaki886@gmail.com>
2016-02-27 17:11:53 +0100

    serwis

--- oglosznie_widok/urls.py
+++ oglosznie_widok/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.regulamin, name='regulamin'),
+    url(r'^$', views.ogloszenie_widok, name='ogloszenie_widok'),
 ]
