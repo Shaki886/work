2c3304c8a8c0da702c982b48b5c0019cedbd78e7
shaki886 <shaki886@gmail.com>
2016-02-27 19:54:02 +0100

    serwis

--- faq/urls.py
+++ faq/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls import url
+# -*- coding: utf-8 -*-
+from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.faq, name='faq'),
+    url(r'^$', views.faq),
 ]
