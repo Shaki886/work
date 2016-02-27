557db0e117d60007bdabbd1d048ef28ff8227753
shaki886 <shaki886@gmail.com>
2016-02-27 14:11:31 +0100

    serwis

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,9 +5,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 admin.autodiscover()
 
-
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('index.urls')),	
-    url(r'^blog/', include('blog.urls')),
+    url(r'', 'index.views.index'),
+    url(r'^blog/', 'blog.views.blog'),
 ]
