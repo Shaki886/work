53ad988b150f0fed118e3f38f50c458c27814e71
Shaki886 <shaki886@gmail.com>
2016-02-28 20:17:22 +0100

    serwis

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from .models import Blog
+from .models import .models
 
 def Blog(request):
 
     blog = Blog.objects.order_by()
 
     return render(request, 'blog/blog.html', {'blogs': blogs})
+
+def Kategorie_aut(request):
+
+    kategorie = Kategorie_aut.objects.order_by()
+
+    return render(request, 'blog/blog.html', {'kategorie': kategorie})
