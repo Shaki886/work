b38eb0b239968cfa6ece2018f64052998c3d0dd4
Shaki886 <shaki886@gmail.com>
2016-02-29 00:07:54 +0100

    work

--- mysite/views.py
+++ mysite/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 from django.contrib import auth
-from django.core.context_processors import csrf
+
 
 def logowanie(request):
 	c = {}
-	c.update(csrf(request))
+
 	return render_to_response('logowanie.html', c)
 	
 def auth_view(request):
