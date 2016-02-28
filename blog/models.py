c9508547d2c12e051ffd84e088fc8e48ff7d2dd6
Shaki886 <shaki886@gmail.com>
2016-02-28 20:45:54 +0100

    serwis

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 from django.utils import timezone
 
-
 class BLOG(models.Model):
 	Login = models.ForeignKey('auth.User')
 	Tytul = models.CharField(max_length=200)
-	Marka = models.TextField(50)
-	Model = models.TextField(50)
+	Marka = models.CharField(max_length=50)
+	Model = models.CharField(max_length=50)
 	Rok = models.IntegerField(4)
 	Stan = models.TextField(default='uzywany')
 	Przebieg = models.IntegerField(50)
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,1 @@
 from django.shortcuts import render
-
-from .models import BLOG
-
-def blog(request):
-
-    blogs = BLOG.objects.order_by()
-
-    return render(request, 'blog/blog.html', {'blogs': blogs})
