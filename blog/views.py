1fe3e2c6cf863545a7ae722484fff290f116500e
Shaki886 <shaki886@gmail.com>
2016-02-28 19:53:45 +0100

    serwis

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .models import Blog
 
 def blog(request):
-    return render(request, 'blog/blog.html', {})
+
+    blog= blog.objects.order_by()
+
+    return render(request, 'blog/blog.html', {'blog': blog})
