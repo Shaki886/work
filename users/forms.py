6355f9d0ebff2a6b7f956677d3835953aa3c7d78
Shaki886 <shaki886@gmail.com>
2016-02-29 14:34:18 +0100

    work

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,24 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+# _*_ coding: utf-8 _*_
+from django import forms
+from bookings.models import User
+from django.core.exceptions import ObjectDoesNotExist
 
-
-class CreateUserForm(UserCreationForm):
-
-    class Meta:
-        model = User
-        fields = ('email', 'password')
+class FormularzLogowania(forms.Form):
+    email = forms.CharField(label="email:",max_length=30)
+    password = forms.CharField(label="Hasło:",widget=forms.PasswordInput())
+    def clean_eamil(self):
+        email = self.cleaned_data['email']
+        try:
+            user = User.objects.get(email=email)
+            return email
+        except ObjectDoesNotExist:
+            raise forms.ValidationError("Niepoprawny email")        
+    def clean_password(self):
+        if self.cleaned_data.has_key('email'):
+            email = self.cleaned_data['email']
+            password = self.cleaned_data['password']
+            user = User.objects.get(email=email)
+            if user.check_password(password):
+                return password
+            raise forms.ValidationError("Niepoprawne hasło")                
+        raise forms.ValidationError("Niepoprawny email")
