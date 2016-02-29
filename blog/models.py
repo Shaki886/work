b5ef142dde6e77373c72f21b4d1ac5d1f2d8fcf4
Shaki886 <shaki886@gmail.com>
2016-02-29 22:57:21 +0100

    work

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from django.utils import timezone
 
+def upload_location(instance, filename):
+	filebase, extension = filename.split(".")
+	return "%s/%s.%s" %{instance.id, filename.id, extension}
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
-    image = models.ImageField(null=True, blank=True, width_field="width_field", height_field="height_field")
+    image = models.ImageField(upload_to=upload_location, null=True, blank=True, width_field="width_field", height_field="height_field")
     width_field = models.IntegerField(default=300)
     height_field = models.IntegerField(default=380)
     created_date = models.DateTimeField(
