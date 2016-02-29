ebe04813b7c3c0eb8c7c627a112b7b46512838ab
Shaki886 <shaki886@gmail.com>
2016-02-29 21:27:51 +0100

    work

--- mysite/settings.py
+++ mysite/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR, "static_cdn")
 
-MEDIA_URL = '/media/'
+MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR, "media_cdn")
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
