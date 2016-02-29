952e146ecaede10db5f065be898c7aab36881bfe
Shaki886 <shaki886@gmail.com>
2016-02-29 18:37:52 +0100

    work

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,18 @@ from .models import Post
 
 def blog(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    paginate_by = 4	
+    	
     return render(request, 'blog/blog.html', {'posts': posts})
+	
+class PostList(ListView):
+    paginate_by = 4 #this variable is used for pagination 
+    def get_queryset(self):
+                 
+        if 'search' in self.request.GET:
+            objects = Post.objects.filter(
+                Q(title__icontains=self.request.GET['search']) | Q(body__icontains=self.request.GET['search'])
+                )
+        else:
+            objects = Post.objects.all()
+         
+        return objects
