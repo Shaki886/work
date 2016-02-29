6420ec713b5283469f32edb3057da4c8f9b09dfc
Shaki886 <shaki886@gmail.com>
2016-02-29 22:41:08 +0100

    work

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,11 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
-    image = models.FileField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True,
+			width_field="width_field",
+			height_field="height_field")
+	width_field = models.IntegerField(default=0)
+	height_field = models.IntegerField(default=0)
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
