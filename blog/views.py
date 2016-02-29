605715db803965fae78f9dd7457c9270bd6b29de
Shaki886 <shaki886@gmail.com>
2016-02-29 17:14:53 +0100

    work

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from django.core.paginator import Paginator
 def blog(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	
-    paginator  Paginator(entries, 4)
+    paginator   Paginator(entries, 4)
     page = paginator.page(1)
 	
     ctx = {'page' : page }
