a62954833f8fe72671c2775741a84b26d6cd18c3
Shaki886 <shaki886@gmail.com>
2016-03-01 00:09:15 +0100

    work

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
 	url(r'^/?$', 'blog.views.post_list', name='post-list'),
 	url(r'^(?P<page>[0-9]+)/$', 'blog.views.post_list', name='post-list'),
-	url(r'^/post/?$', 'blog.views.post', name='post'),
-	url(r'^/post/(?P<page>[0-9]+)/$', 'blog.views.post', name='post'),
+	url(r'^post/?$', 'blog.views.post', name='post'),
+	url(r'^post/(?P<page>[0-9]+)/$', 'blog.views.post', name='post'),
 	]
