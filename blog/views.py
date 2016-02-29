9a7340c051a86910eedbb03022f5d37cdef924e7
Shaki886 <shaki886@gmail.com>
2016-02-29 18:49:44 +0100

    work

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,11 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.views import generic
 
-def blog(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    	
-    return render(request, 'blog/blog.html', {'posts': posts})
-	
-class PostList(ListView):
-    paginate_by = 4 #this variable is used for pagination 
-    def get_queryset(self):
-                 
-        if 'search' in self.request.GET:
-            objects = Post.objects.filter(
-                Q(title__icontains=self.request.GET['search']) | Q(body__icontains=self.request.GET['search'])
-                )
-        else:
-            objects = Post.objects.all()
-         
-        return objects
+class NewsList(generic.ListView):
+    model = models.Post
+    paginate_by = 10
+    context_object_name = 'post_list'
+
+post_list = NewsList.as_view()
