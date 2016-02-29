d5cbff2379f5d41421b5afc28108971c68c5c224
Shaki886 <shaki886@gmail.com>
2016-02-29 17:10:40 +0100

    work

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.core.paginator import Paginator
 
 def blog(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	
+    paginator  Paginator(entries, 4)
+    page = paginator.page(1)
+	
+    ctx = {'page' : page }
+	
     return render(request, 'blog/blog.html', {'posts': posts})
