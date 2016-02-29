656a88a8eae6d13ce3bb446654e4a90b195582c8
Shaki886 <shaki886@gmail.com>
2016-02-29 22:02:50 +0100

    work

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,10 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR, "static_cdn"))
+STATIC_ROOT = os.path.join (BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR, "media_cdn"))
+MEDIA_ROOT = os.path.join (BASE_DIR, 'media')
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'fake_emails')
