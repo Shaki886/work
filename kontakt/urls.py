6e472a2a199f894c6aeb45b34a629d2fb98ae594
Shaki886 <shaki886@gmail.com>
2016-03-01 01:06:29 +0100

    work

--- kontakt/urls.py
+++ kontakt/urls.py
@@ -1,24 +1,6 @@
-"""
-Example URLConf for a contact form.
-
-If all you want is the basic ContactForm with default behavior, just
-include this URLConf somewhere in your URL hierarchy (for example, at
-``/contact/``)>
-
-"""
-
 from django.conf.urls import url
-from django.views.generic import TemplateView
-
-from kontakt.views import ContactFormView
-
+from . import views
 
 urlpatterns = [
-    url(r'^$',
-        ContactFormView.as_view(),
-        name='contact_form'),
-    url(r'^sent/$',
-        TemplateView.as_view(
-            template_name='contact_form/contact_form_sent.html'),
-        name='contact_form_sent'),
+    url(r'^$', views.kontakt, name='kontakt'),
 ]
