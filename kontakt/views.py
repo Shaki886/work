550ff405189c2f7050504afc0e3c8460f4c05623
Shaki886 <shaki886@gmail.com>
2016-03-01 01:02:58 +0100

    work

--- kontakt/views.py
+++ kontakt/views.py
@@ -1,39 +1,61 @@
-# -*- coding: utf-8 -*-
-from django.core.mail import send_mail
-from django.http import HttpResponse, HttpResponseRedirect
-from django import forms
-from django.shortcuts import render_to_response
- 
-class ContactForm(forms.Form):
-	subject = forms.CharField(max_length=255, label=u'Temat', required=True, widget=forms.TextInput(attrs={'size':'40'}))
-	msg_content = forms.CharField(widget=forms.Textarea, label=u'Treść wiadomości', required=True)
-	sender = forms.EmailField(label=u'Twój adres email', required=True, widget=forms.TextInput(attrs={'size':'40'}))
-	cc_sender = forms.BooleanField(required=False, label=u'Wyślij mi kopię tego emaila')
- 
-def contact_form(request):
-	if request.method == 'POST':
-		form = ContactForm(request.POST)
-		if form.is_valid():
-			subject = form.cleaned_data['subject']
-			msg_content = form.cleaned_data['msg_content']
-			sender = form.cleaned_data['sender']
-			cc_sender = form.cleaned_data['cc_sender']
-			odbiorcy = ['example@example.example']
-			if cc_sender:
-				odbiorcy.append(sender)
-			send_mail(subject, msg_content, sender, odbiorcy)
-			return HttpResponseRedirect('/contact/success/')
-	else:
-		form = ContactForm()
- 
-	return render_to_response('kontakt.html', {'form': form})
- 
-def contact_ok(request):
-	try:
-		referer = request.META['HTTP_REFERER']
-	except KeyError:
-		return HttpResponseRedirect('/contact/')
-	else:
-		if referer == 'http://adres/contact/':
-			return render_to_response('contact_ok.html', {})
-	return HttpResponseRedirect('/contact/')
+"""
+View which can render and send email from a contact form.
+
+"""
+
+from django.core.urlresolvers import reverse
+from django.views.generic.edit import FormView
+
+from .forms import ContactForm
+
+
+class ContactFormView(FormView):
+    form_class = ContactForm
+    recipient_list = None
+    template_name = 'contact_form/contact_form.html'
+
+    def form_valid(self, form):
+        form.save()
+        return super(ContactFormView, self).form_valid(form)
+
+    def form_invalid(self, form):
+        # tl;dr -- this method is implemented to work around Django
+        # ticket #25548, which is present in the Django 1.9 release
+        # (but not in Django 1.8).
+        #
+        # The longer explanation is that in Django 1.9,
+        # FormMixin.form_invalid() does not pass the form instance to
+        # get_context_data(). This causes get_context_data() to
+        # construct a new form instance with the same data in order to
+        # put it into the template context, and then any access to
+        # that form's ``errors`` or ``cleaned_data`` runs that form
+        # instance's validation. The end result is that validation
+        # gets run twice on an invalid form submission, which is
+        # undesirable for performance reasons.
+        #
+        # Manually implementing this method, and passing the form
+        # instance to get_context_data(), solves this issue (which was
+        # fixed in Django 1.9.1 and will not be present in Django
+        # 1.10).
+        return self.render_to_response(self.get_context_data(form=form))
+
+    def get_form_kwargs(self):
+        # ContactForm instances require instantiation with an
+        # HttpRequest.
+        kwargs = super(ContactFormView, self).get_form_kwargs()
+        kwargs.update({'request': self.request})
+
+        # We may also have been given a recipient list when
+        # instantiated.
+        if self.recipient_list is not None:
+            kwargs.update({'recipient_list': self.recipient_list})
+        return kwargs
+
+    def get_success_url(self):
+        # This is in a method instead of the success_url attribute
+        # because doing it as an attribute would involve a
+        # module-level call to reverse(), creating a circular
+        # dependency between the URLConf (which imports this module)
+        # and this module (which would need to access the URLConf to
+        # make the reverse() call).
+        return reverse('contact_form_sent')
