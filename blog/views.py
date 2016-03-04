4090d72e7985cad75a8426b332ec88119e13db7f
Shaki886 <shaki886@gmail.com>
2016-03-04 17:56:30 +0100

    work

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 
@@ -7,12 +8,22 @@ from .models import Post
 
 
 def post_list(request):
-	queryset = Post.objects.all().order_by("-created_date")
-	context = {
+	queryset_list = Post.objects.all().order_by("-created_date")
+    paginator = Paginator(queryset_list, 4) 
+
+    page = request.GET.get('page')
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        queryset = paginator.page(1)
+    except EmptyPage:
+        queryset = paginator.page(paginator.num_pages)
+    context = {
 		"post_list": queryset,
 		"title": "View"
 		}
 	return render(request, "post_list.html", context)
+
 	
 def post_view(request, id=None):
 	instance = get_object_or_404(Post, id=id)
