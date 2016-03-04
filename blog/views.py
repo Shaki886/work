3c48eadda7a693c8d402756de3648546d53afdd6
Shaki886 <shaki886@gmail.com>
2016-03-04 17:10:10 +0100

    work

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,23 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
+
+
 from .models import Post
 
 
-def post_view(request):
+def post_list(request):
 	queryset = Post.objects.all()
 	context = {
 		"post_list": queryset,
 		"title": "View"
 		}
-	paginate_by = 1
+	return render(request, "post_list.html", context)
+	
+def post_view(request):
+	instance = get_object_or_404(Post, id=3)
+	context = {
+		"title": "Detail"
+		"instance": instance,
+		}
 	return render(request, "view.html", context)
-	
-	
