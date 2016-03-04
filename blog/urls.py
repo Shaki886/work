2b80cbc25a134a4ffeaa8a63dc324e152c4f695a
Shaki886 <shaki886@gmail.com>
2016-03-04 17:21:18 +0100

    work

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
 	url(r'^/?$', 'blog.views.post_list', name='post-list'),
 	url(r'^(?P<page>[0-9]+)/$', 'blog.views.post_list', name='post-list'),
-	url(r'^view/$', 'blog.views.post_view', name='post-view'),
+	url(r'^view/(?P<id>\d+)$/', 'blog.views.post_view', name='post-view'),
 	]
--- blog/views.py
+++ blog/views.py
@@ -14,10 +14,10 @@ def post_list(request):
 		}
 	return render(request, "post_list.html", context)
 	
-def post_view(request):
-	instance = get_object_or_404(Post, id=3)
+def post_view(request, id=None):
+	instance = get_object_or_404(Post, id=id)
 	context = {
-		"title": "Detail",
+		"title": instance.title,
 		"instance": instance,
 		}
 	return render(request, "view.html", context)
