554ad4ef80291fd432bf3f1a906a2e059076cb56
Shaki886 <shaki886@gmail.com>
2016-03-04 13:42:50 +0100

    work

--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,10 @@ class PostList(generic.ListView):
     context_object_name = 'post_list'
 
 post_list = PostList.as_view()
+
+class ViewList(generic.ListView):
+    model = models.Post
+    paginate_by = 1
+    context_object_name = 'view_list'
+
+view_list = ViewList.as_view()
