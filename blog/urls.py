85e8e7240ad67b05a6bccd4ac24b546ce9add4f2
Shaki886 <shaki886@gmail.com>
2016-03-04 13:59:59 +0100

    work

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^/?$', 'blog.views.post_list', name='post-list'),
-	url(r'^(?P<page>[0-9]+)/$', 'blog.views.post_list', name='post-list'),
+	url(r'^/?$', views.PostList.as_view(), name='post-list'),
+	url(r'^(?P<page>[0-9]+)/$', views.PostList.as_view(), name='post-list'),
 	url(r'^post/(?P<page>[0-9]+)/$', views.ViewList.as_view(), name='view-list'),
 	]
