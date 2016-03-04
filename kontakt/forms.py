1e2ab1fb22ffbda96362fa9c862d241a1de59cf0
Shaki886 <shaki886@gmail.com>
2016-03-04 21:32:11 +0100

    work

--- kontakt/forms.py
+++ kontakt/forms.py
@@ -2,11 +2,16 @@ from django import forms
 
 class KontaktForm(forms.Form):
     title = forms.CharField(required=True)
-    email = forms.EmailField(required=True)
     text = forms.CharField(
         required=True,
         widget=forms.Textarea
     )
+    name = forms.Charfield(required=True)
+    email = forms.EmailField(required=True)
+    numer_ogloszenia = forms.IntegerField(required=True)
+    telefon = forms.IntegerField() 
+	
+	
 	
 	
 	
