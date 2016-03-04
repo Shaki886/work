71c8d35f764f33d24453eb26cb8f70648e79b039
Shaki886 <shaki886@gmail.com>
2016-03-04 16:49:06 +0100

    work

--- blog/views.py
+++ blog/views.py
@@ -4,11 +4,13 @@ from django.views import generic
 from .models import Post
 
 
-
 def post_view(request):
 	queryset = Post.objects.all()
+	paginate_by = 1
 	context = {
 		"object_list": queryset,
 		"title": "View"
 		}
 	return render(request, "view.html", context)
+	
+	
