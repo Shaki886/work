eb9840681f3bfde30699bf06a823b6b41da76c16
Shaki886 <shaki886@gmail.com>
2016-03-04 20:19:13 +0100

    work

--- kontakt/views.py
+++ kontakt/views.py
@@ -3,8 +3,8 @@ from kontakt.forms import KontaktForm
 from django.template import RequestContext
 
 def kontakt(request):
+	kontakt_forms = KontaktForm(request.POST or None)
 	if request.method == "POST":
-		kontakt_forms = KontaktForm(request.POST or None)
 		if kontakt_form.is_valid():
 			success = True
 			email = kontakt_form.cleaned_data['email']
