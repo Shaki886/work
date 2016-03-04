c98af737f147c1c2024efb7c25562613c4bb7921
Shaki886 <shaki886@gmail.com>
2016-03-04 20:27:55 +0100

    work

--- kontakt/views.py
+++ kontakt/views.py
@@ -3,17 +3,15 @@ from django.core.mail import send_mail
 from .forms import KontaktForm
 
 def kontakt(request):
-    kontakt_form = KontaktForm
-    # if request is not post, initialize an empty form
-    form = kontakt_form(request.POST or None)
-    if request.method == 'POST':
-
-        if form.is_valid():
+	kontakt_form = KontaktForm
+	# if request is not post, initialize an empty form
+	form = kontakt_form(request.POST or None)
+	if request.method == 'POST':
+		if form.is_valid():
 			email = kontakt_form.cleaned_data['email']
 			title = kontakt_form.cleaned_data['title']
 			text = kontakt_form.cleaned_data['text']
-
-            send_mail('Subject here', msg, email, ['testmail@gmail.com'], fail_silently=False)
-            return HttpResponseRedirect('blog/kontakt/')
-    return render(request, 'index', {'form': form})
+			send_mail('Subject here', msg, email, ['testmail@gmail.com'], fail_silently=False)
+			return HttpResponseRedirect('blog/kontakt/')
+	return render(request, 'index', {'form': form})
 
