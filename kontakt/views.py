07c5a62e6110264f234ded684b056356ca86bdb1
Shaki886 <shaki886@gmail.com>
2016-03-04 19:27:52 +0100

    work

--- kontakt/views.py
+++ kontakt/views.py
@@ -3,16 +3,15 @@ from django.shortcuts import render_toresponse
 from kontakt.forms import KontaktForm
 
 def kontakt(request):
-
-    if request.methof == "POST":
+	if request.methof == "POST":
 		kontakt_forms = KontaktForm(request.POST)
 		if kontact_form.is_valid():
 			success = True
 			email = kontact_form.cleaned_data['email']
 			title = kontact_form.cleaned_data['title']
 			text = kontact_form.cleaned_data['text']
-	else:
-		kontact_form = KontactForm()
-		
-	ctx = {'kontakt_form':contact_form}
-    return render_to_response(request, 'kontakt/kontakt.html', {})
+		else:
+			kontact_form = KontactForm()
+
+	ctx = {'kontakt_form'}\:contact_form}
+	return render_to_response(request, 'kontakt/kontakt.html', {})
