51f96cd6cb618dd9658aa33e556fc56258149419
Shaki886 <shaki886@gmail.com>
2016-03-04 21:51:35 +0100

    work

--- kontakt/forms.py
+++ kontakt/forms.py
@@ -7,7 +7,7 @@ class KontaktForm(forms.Form):
         (INDYWIDUALNY, 'Indywidualny'),
         (DEALER, 'Dealer'),
         )
-    Rodzaj_klienta = models.CharField(max_length=2,
+    Rodzaj_klienta = forms.CharField(max_length=2,
                                       choices=RODZAJ_KLIENTA_CHOICES,
                                       default=INDYWIDUALNY)
     title = forms.CharField(required=True)
