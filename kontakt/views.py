c5f6065cd397c620f2ac404e3b82c8b73cfa4907
Shaki886 <shaki886@gmail.com>
2016-03-04 20:24:47 +0100

    work

--- kontakt/views.py
+++ kontakt/views.py
@@ -1,20 +1,19 @@
-from django.shortcuts import render
-from kontakt.forms import KontaktForm
-from django.template import RequestContext
+from django.shortcuts import render, redirect, get_object_or_404, HttpResponseRedirect, render_to_response
+from django.core.mail import send_mail
+from .forms import KontaktForm
 
 def kontakt(request):
-	kontakt_forms = KontaktForm(request.POST or None)
-	if request.method == "POST":
-		if kontakt_form.is_valid():
-			success = True
+    kontakt_form = KontaktForm
+    # if request is not post, initialize an empty form
+    form = kontakt_form(request.POST or None)
+    if request.method == 'POST':
+
+        if form.is_valid():
 			email = kontakt_form.cleaned_data['email']
 			title = kontakt_form.cleaned_data['title']
 			text = kontakt_form.cleaned_data['text']
-		else:
-			kontakt_form = KontactForm()
 
-
-	ctx = {'kontakt_form':kontakt_form}
-	
-	return render(request, 'kontakt/kontakt.html', ctx, context_instance=RequestContext(request))
+            send_mail('Subject here', msg, email, ['testmail@gmail.com'], fail_silently=False)
+            return HttpResponseRedirect('blog/kontakt/')
+    return render(request, 'index', {'form': form})
 
