42e356600487cf03780d658a80ef4af183c7c455
Shaki886 <shaki886@gmail.com>
2016-03-04 19:25:20 +0100

    work

--- kontakt/forms.py
+++ kontakt/forms.py
@@ -1,10 +1,10 @@
 from django import forms
 
 class KontaktForm(forms.Form):
-
-	tresc = forms.Charfield( widget=forms.Textarea)
-
 	e-mail = forms.EmailField()
+	title = forms.CharField()
+	text = forms.Charfield( widget=forms.Textarea)
+
 
 	
 	
--- kontakt/views.py
+++ kontakt/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render_toresponse
 
 from kontakt.forms import KontaktForm
 
 def kontakt(request):
-    return render(request, 'kontakt/kontakt.html', {})
+
+    if request.methof == "POST":
+		kontakt_forms = KontaktForm(request.POST)
+		if kontact_form.is_valid():
+			success = True
+			email = kontact_form.cleaned_data['email']
+			title = kontact_form.cleaned_data['title']
+			text = kontact_form.cleaned_data['text']
+	else:
+		kontact_form = KontactForm()
+		
+	ctx = {'kontakt_form':contact_form}
+    return render_to_response(request, 'kontakt/kontakt.html', {})
