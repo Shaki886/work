5bf96664dd33cf3a93470eb188ab8d59faffe3bb
Shaki886 <shaki886@gmail.com>
2016-03-04 14:59:56 +0100

    work

--- blog/views.py
+++ blog/views.py
@@ -3,12 +3,12 @@ from django.shortcuts import render
 from django.views import generic
 from blog import models
 
-def view_list(request);
-	return HttpResponse("<h1>Hello</h1>")
-
 class PostList(generic.ListView):
     model = models.Post
     paginate_by = 4
     context_object_name = 'post_list'
 
 post_list = PostList.as_view()
+
+def view_list(request):
+	return HttpResponse("<h1>Hello</h1>")
