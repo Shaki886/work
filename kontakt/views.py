1c99a148004fe81735d7d20fe38cef90c42dea93
Shaki886 <shaki886@gmail.com>
2016-03-04 19:33:25 +0100

    work

--- kontakt/views.py
+++ kontakt/views.py
@@ -5,13 +5,13 @@ from kontakt.forms import KontaktForm
 def kontakt(request):
 	if request.method == "POST":
 		kontakt_forms = KontaktForm(request.POST)
-		if kontact_form.is_valid():
+		if kontakt_form.is_valid():
 			success = True
-			email = kontact_form.cleaned_data['email']
-			title = kontact_form.cleaned_data['title']
-			text = kontact_form.cleaned_data['text']
+			email = kontakt_form.cleaned_data['email']
+			title = kontakt_form.cleaned_data['title']
+			text = kontakt_form.cleaned_data['text']
 		else:
-			kontact_form = KontactForm()
+			kontakt_form = KontactForm()
 
-	ctx = {'kontakt_form':kontact_form}
+	ctx = {'kontakt_form':kontakt_form}
 	return render_to_response(request, 'kontakt/kontakt.html', {})
