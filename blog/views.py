ef767b786e1e410e4c802532ee1a65e7d06f525d
Shaki886 <shaki886@gmail.com>
2016-03-04 16:46:26 +0100

    work

--- blog/views.py
+++ blog/views.py
@@ -3,12 +3,7 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Post
 
-class PostList(generic.ListView):
-    model = models.Post
-    paginate_by = 4
-    context_object_name = 'post_list'
 
-post_list = PostList.as_view()
 
 def post_view(request):
 	queryset = Post.objects.all()
