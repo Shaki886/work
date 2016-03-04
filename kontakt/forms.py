f6d4fd27f9601b3b6bfb0f3054e815b0c3dfe61a
Shaki886 <shaki886@gmail.com>
2016-03-04 20:45:26 +0100

    work

--- kontakt/forms.py
+++ kontakt/forms.py
@@ -1,11 +1,12 @@
 from django import forms
 
 class KontaktForm(forms.Form):
-	email = forms.EmailField()
-	title = forms.CharField()
-	text = forms.CharField( widget=forms.Textarea)
-
-
+    title = forms.CharField(required=True)
+    email = forms.EmailField(required=True)
+    text = forms.CharField(
+        required=True,
+        widget=forms.Textarea
+    )
 	
 	
 	
