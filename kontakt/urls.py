b2f69249305a2670022644abd921731ff3f7b29b
Shaki886 <shaki886@gmail.com>
2016-03-04 21:05:07 +0100

    work

--- kontakt/urls.py
+++ kontakt/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.kontakt, name='kontakt'),
-    url(r'^ok/$', {'template_name': 'ok.html'}, name='kontaktok'),
+	url(r'^/?$', 'kontakt.views.kontakt', name='kontakt'),
+	url(r'^ok/?$', 'kontakt.views.ok', name='kontaktok'),
 ]
