4be701954ee7a1a76d687b3d4527aaf8673fb187
Shaki886 <shaki886@gmail.com>
2016-03-04 19:31:41 +0100

    work

--- kontakt/forms.py
+++ kontakt/forms.py
@@ -3,7 +3,7 @@ from django import forms
 class KontaktForm(forms.Form):
 	email = forms.EmailField()
 	title = forms.CharField()
-	text = forms.Charfield( widget=forms.Textarea)
+	text = forms.CharField( widget=forms.Textarea)
 
 
 	
