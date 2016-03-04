214882339a6036a993144ed4e00208d99f6c7026
Shaki886 <shaki886@gmail.com>
2016-03-04 21:42:13 +0100

    work

--- kontakt/forms.py
+++ kontakt/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from django.db import models
 
 class KontaktForm(forms.Form):
     title = forms.CharField(required=True)
@@ -10,16 +9,10 @@ class KontaktForm(forms.Form):
     name = forms.CharField(required=True)
     email = forms.EmailField(required=True)
     numer_ogloszenia = forms.IntegerField(required=True)
-    telefon = forms.IntegerRangeField(min_value=9, max_value=12) 
+    telefon = forms.CharField(min_length=9) 
+	
+	
 
-class IntegerRangeField(models.IntegerField):
-    def __init__(self, verbose_name=None, name=None, min_value=None, max_value=None, **kwargs):
-        self.min_value, self.max_value = min_value, max_value
-        models.IntegerField.__init__(self, verbose_name, name, **kwargs)
-    def formfield(self, **kwargs):
-        defaults = {'min_value': self.min_value, 'max_value':self.max_value}
-        defaults.update(kwargs)
-        return super(IntegerRangeField, self).formfield(**defaults)
 	
 	
 	
