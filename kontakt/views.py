efb9b3059dc8b612ebe89f89fccbb1c2b34d4424
Shaki886 <shaki886@gmail.com>
2016-03-05 00:55:00 +0100

    work

--- kontakt/views.py
+++ kontakt/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404, HttpResponseRedirect, render_to_response
 from django.core.mail import send_mail
 from .forms import KontaktForm
+from .models import TEMAT
 
 def kontakt(request):
 	form_class = KontaktForm
@@ -18,4 +19,10 @@ def kontakt(request):
 			send_mail('Subject here', text, email, ['testmail@gmail.com'], fail_silently=False)
 			return HttpResponseRedirect('/kontaktok/')
 	return render(request, 'kontakt/kontakt.html', {'form': form})
+
+
+
+def temat(request):
+
+    temats= TEMAT.objects.order_by()
 
