73f7554470bd6eb17ffce864f773af6d82b94e0a
Shaki886 <shaki886@gmail.com>
2016-03-04 14:09:33 +0100

    work

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,14 @@
-from django.views import generic
 from blog import models
+from django.views.generic.edit import CreateView
 
-class PostList(generic.ListView):
+class PostList(CreateView):
     model = models.Post
     paginate_by = 4
     context_object_name = 'post_list'
     template_name = 'post_list.html'
 
 
-class ViewList(generic.ListView):
+class ViewList(CreateView):
     model = models.Post
     paginate_by = 1
     context_object_name = 'view_list'
