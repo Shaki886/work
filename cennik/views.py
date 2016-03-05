4f4a7197ab3ef66716f23e0631a27fd290e6584a
Shaki886 <shaki886@gmail.com>
2016-03-05 01:39:04 +0100

    work

--- cennik/views.py
+++ cennik/views.py
@@ -5,18 +5,35 @@ from .models import CENNIK_IND
 from .models import CENNIK_DEA
 from .models import DODATKI
 
-def cennik(request):
-
-    cennik_ind = CENNIK_IND.objects.order_by()
-    cennik_dea = CENNIK_DEA.objects.order_by()
-    dodatki = DODATKI.objects.order_by()
-
-    return render(request, 'cennik/cennik.html', {'cennik': cennik})
 	
 def dni_list(request):
-	queryset_list = DNI.objects.all().order_by("-created_date")
+	queryset_list = DNI.objects.all()
 	context = {
 		"dni_list": queryset,
+		"title": "View"
+		}
+	return render(request, "cennik.html", context)
+	
+def cenni_ind_list(request):
+	queryset_list = CENNIK_IND.objects.all()
+	context = {
+		"cenni_ind": queryset,
+		"title": "View"
+		}
+	return render(request, "cennik.html", context)
+	
+def cennik_dea_list(request):
+	queryset_list = CENNIK_DEA.objects.all()
+	context = {
+		"cenni_dea": queryset,
+		"title": "View"
+		}
+	return render(request, "cennik.html", context)
+	
+def dodatki_list(request):
+	queryset_list = DODATKI.objects.all()
+	context = {
+		"dodatki_list": queryset,
 		"title": "View"
 		}
 	return render(request, "cennik.html", context)
