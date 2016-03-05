b41a14206866a8f02775dbc1e21738558c0c302d
Shaki886 <shaki886@gmail.com>
2016-03-05 01:16:28 +0100

    work

--- cennik/views.py
+++ cennik/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
 
+from .models import DNI
+from .models import CENNIK_IND
+from .models import CENNIK_DEA
+from .models import DODATKI
+
 def cennik(request):
-    return render(request, 'cennik/cennik.html', {})
+
+    dnis = DNI.objects.order_by()
+    cennik_ind = CENNIK_IND.objects.order_by()
+    cennik_dea = CENNIK_DEA.objects.order_by()
+    dodatki = DODATKI.objects.order_by()
+
+    return render(request, 'cennik/cennik.html', {'regulamins': regulamins})
