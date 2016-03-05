da9480958b7e449175b2aebbbc93c6f4c9777b23
Shaki886 <shaki886@gmail.com>
2016-03-05 01:29:31 +0100

    work

--- cennik/models.py
+++ cennik/models.py
@@ -13,10 +13,8 @@ class CENNIK_IND(models.Model):
 	kolumna3 =models.CharField(max_length=200)
 	
 class CENNIK_DEA(models.Model):
-	szczegoly = models.CharField(max_length=200)
 	kolumna1 =models.CharField(max_length=200)
 	kolumna2 =models.CharField(max_length=200)
-	kolumna3 =models.CharField(max_length=200)
 	
 class DODATKI(models.Model):
 	dodatki = models.CharField(max_length=500)
--- cennik/views.py
+++ cennik/views.py
@@ -12,4 +12,4 @@ def cennik(request):
     cennik_dea = CENNIK_DEA.objects.order_by()
     dodatki = DODATKI.objects.order_by()
 
-    return render(request, 'cennik/cennik.html', {'dnis': dni})
+    return render(request, 'cennik/cennik.html', {'cennik': cennik})
