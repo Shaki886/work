cc5cdbf329640cd24f75ef9ab75cee2b45924510
Shaki886 <shaki886@gmail.com>
2016-03-05 01:18:39 +0100

    work

--- cennik/admin.py
+++ cennik/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 
-from .models import CENNIK
-admin.site.register(CENNIK)
+from .models import DNI
+admin.site.register(DNI)
+
+from .models import CENNIK_IND
+admin.site.register(CENNIK_IND)
+
+from .models import CENNIK_DEA)
+admin.site.register(CENNIK_DEA)
+
+from .models import DODATKI
+admin.site.register(DODATKI)
