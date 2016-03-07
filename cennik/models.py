d696e8692038d9c294e6ed01f3df1a151960cd5f
Shaki886 <shaki886@gmail.com>
2016-03-07 15:00:56 +0100

    work

--- cennik/models.py
+++ cennik/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
-class Indywidualny(models.model):
+class Indywidualny(models.Model):
 	numer_wiersza = models.IntegerField()
 	wiersz = models.CharField(max_length=100)
