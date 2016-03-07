fb1a6bc01fd81b319ef829a97c4523e8bf5fccfa
Shaki886 <shaki886@gmail.com>
2016-03-07 18:33:52 +0100

    work

--- kontakt/models.py
+++ kontakt/models.py
@@ -2,7 +2,7 @@
 from django.db import models
 
 class KontaktForm(models.Model):
-    klient = models.CharField(max_length=30))
+    klient = models.CharField(max_length=30)
     tytul = models.CharField(max_length=30)
     tekst = models.TextField()
     imie_i_nazwisko = models.CharField(max_length=60)
