4af85f9b9495109bb6914995383b12a1efe01f8c
Shaki886 <shaki886@gmail.com>
2016-03-07 18:28:52 +0100

    work

--- kontakt/admin.py
+++ kontakt/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 
+from .models import KontaktForm
 from .models import Temat
+
 admin.site.register(Temat)
+admin.site.register(KontaktForm)
--- kontakt/models.py
+++ kontakt/models.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from django.db import models
-from django import forms
 
 class KontaktForm(models.Model):
     klient = forms.CharField(required=False)
