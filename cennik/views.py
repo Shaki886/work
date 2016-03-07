9b3149af7d389ce36ba253c3cee1143862fe928e
Shaki886 <shaki886@gmail.com>
2016-03-07 16:22:55 +0100

    work

--- cennik/views.py
+++ cennik/views.py
@@ -2,18 +2,13 @@ from django.shortcuts import render
 from .models import Indywidualny
 from .models import Naglowek
 
-def naglowek_list (request):
-	queryset = Naglowek.objects.all()
-	context = {
-		"naglowek_list": queryset,
-		"title": "View"
-		}
-	return render(request, 'cennik.html', context)
 
 def indywidualny_list (request):
-	queryset = Indywidualny.objects.all()
+	queryset_naglowek = Naglowek.objects.all()
+	queryset_indywidualny = Indywidualny.objects.all()
 	context = {
-		"indywidualny_list": queryset,
+		"indywidualny_list": queryset_indywidualny,
+		"naglowek_list": queryset_naglowek,		
 		"title": "View"
 		}
 	return render(request, 'cennik.html', context)
