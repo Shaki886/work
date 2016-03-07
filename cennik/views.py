ade097846ccf5d5ca9f064e42c167a8ab32e344a
Shaki886 <shaki886@gmail.com>
2016-03-07 15:29:55 +0100

    work

--- cennik/views.py
+++ cennik/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .models import Indywidualny
 
-def cennik(request):
-    indywidualnys = Indywidualny.objects.filter
-    return render(request, 'cennik/cennik.html', {'indywidualnys':indywidualnys})
+def indywidualny_list (request):
+	queryset = Post.objects.all()
+	context = {
+		"indywidualny_list": queryset,
+		}
+    return render(request, 'cennik/cennik.html', context)
