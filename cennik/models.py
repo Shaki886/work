9fc3aa76ccb8dbfd10c406a63c67efbddff8cbe7
Shaki886 <shaki886@gmail.com>
2016-03-07 16:05:49 +0100

    work

--- cennik/models.py
+++ cennik/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 
+class IndywidualnyNaglowek(models.Model):
+	numer_wiersza = models.CharField(max_length=10)
+	kolumna_1 = models.CharField(max_length=100)
+	kolumna_2 = models.CharField(max_length=100)
+	kolumna_3 = models.CharField(max_length=100)
+	kolumna_4 = models.CharField(max_length=100)
+
+	
+	def __str__(self):
+		return self.numer_wiersza
+		
 class Indywidualny(models.Model):
 	numer_wiersza = models.CharField(max_length=10)
 	kolumna_1 = models.CharField(max_length=100)
--- cennik/views.py
+++ cennik/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from .models import Indywidualny
+from .models import IndywidualnyNaglowek
 
 def indywidualny_list (request):
 	queryset = Indywidualny.objects.all()
 	context = {
 		"indywidualny_list": queryset,
+		"title": "View"
+		}
+	return render(request, 'cennik.html', context)
+	
+def indywidualnynaglowek_list (request):
+	queryset = IndywidualnyNaglowek.objects.all()
+	context = {
+		"indywidualnynaglowek_list": queryset,
 		"title": "View"
 		}
 	return render(request, 'cennik.html', context)
