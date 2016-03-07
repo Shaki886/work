b8dc2eee00dae481ff4258265acbe64c3e8aad75
Shaki886 <shaki886@gmail.com>
2016-03-07 18:27:04 +0100

    work

--- kontakt/forms.py
+++ kontakt/forms.py
@@ -1,13 +1,5 @@
 from django import forms
-
-class KontaktForm(forms.Form):
-    klient = forms.CharField(required=False)
-    tytul = forms.CharField(required=False)
-    tekst = forms.CharField(
-        required=True,
-        widget=forms.Textarea
-    )
-    imie_i_nazwisko = forms.CharField(required=True)
-    email = forms.EmailField(required=True)
-    numer_ogloszenia = forms.CharField(required=True)
-    telefon = forms.CharField(min_length=9)
+	
+class KontaktForm(forms.ModelForm):
+    class Meta:
+        model = Kontakt
--- kontakt/models.py
+++ kontakt/models.py
@@ -1,5 +1,18 @@
 # -*- coding: utf-8 -*-
 from django.db import models
+from django import forms
+
+class KontaktForm(models.Model):
+    klient = forms.CharField(required=False)
+    tytul = forms.CharField(required=False)
+    tekst = forms.CharField(
+        required=True,
+        widget=forms.Textarea
+    )
+    imie_i_nazwisko = forms.CharField(required=True)
+    email = forms.EmailField(required=True)
+    numer_ogloszenia = forms.CharField(required=True)
+    telefon = forms.CharField(min_length=9)
 
 class Temat(models.Model):
 	temat = models.CharField(max_length=100)
