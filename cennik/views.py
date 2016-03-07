f24837bb3096a10c8493a2e07c2056b719d4dc05
Shaki886 <shaki886@gmail.com>
2016-03-07 15:06:28 +0100

    work

--- cennik/views.py
+++ cennik/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Indywidualny
 
 def cennik(request):
+    indywidualnys = Indywidualny.objects.filter
     return render(request, 'cennik/cennik.html', {})
