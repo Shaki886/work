0f0e8861258add1cce781ce04ca5331dfe6c742d
Shaki886 <shaki886@gmail.com>
2016-03-07 18:43:11 +0100

    work

--- kontakt/forms.py
+++ kontakt/forms.py
@@ -3,4 +3,4 @@ from .models import Kontakt
 class KontaktForm(forms.ModelForm):
     class Meta:
         model = Kontakt
-		fields = ('klient', 'tytul', 'tekst', 'imie_i_nazwisko', 'email', 'numer_ogloszenia', 'telefon')
+		fields = ('klient', 'tytul', 'tekst', 'imie_i_nazwisko', 'email', 'numer_ogloszenia', 'telefon',)
