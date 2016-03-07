4854b282c006c688b421da31fea221e7b95b5bb1
Shaki886 <shaki886@gmail.com>
2016-03-07 16:09:26 +0100

    work

--- cennik/admin.py
+++ cennik/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Indywidualny
+from .models import IndywidualnyNaglowek
 
 admin.site.register(Indywidualny)
+admin.site.register(IndywidualnyNaglowek)
