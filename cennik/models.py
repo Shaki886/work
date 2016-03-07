b46806bd69ecd40faaaa73813adc7997ed107634
Shaki886 <shaki886@gmail.com>
2016-03-07 15:47:15 +0100

    work

--- cennik/models.py
+++ cennik/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 class Indywidualny(models.Model):
 	numer_wiersza = models.CharField()
-	wiersz = models.CharField(max_length=100)
+	kolumna_1 = models.CharField(max_length=100)
+	kolumna_2 = models.CharField(max_length=100)
+	kolumna_3 = models.CharField(max_length=100)
+	kolumna_4 = models.CharField(max_length=100)
 
 	
 	def __str__(self):
