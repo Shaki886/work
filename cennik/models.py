e8c637fe2dbc44d5da207436601917c73c07624c
Shaki886 <shaki886@gmail.com>
2016-03-07 16:24:44 +0100

    work

--- cennik/models.py
+++ cennik/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class Naglowek(models.Model):
-	numer_wiersza = models.CharField(max_length=10)
 	kolumna_1 = models.CharField(max_length=100)
 	kolumna_2 = models.CharField(max_length=100)
 	kolumna_3 = models.CharField(max_length=100)
